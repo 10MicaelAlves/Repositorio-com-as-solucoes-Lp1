0333796d39ba00e4dba7c903423a62b4dee486e6
10MicaelAlves <micaelalvesmesquita@gmail.com>
2025-09-02 11:28:41 -0300

    Update temperaturas.c
    Signed-off-by: 10MicaelAlves <micaelalvesmesquita@gmail.com>

--- solucoes-slide-3/tipos-de-dados.md/temperaturas.c
+++ solucoes-slide-3/tipos-de-dados.md/temperaturas.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    signed char temperaturas[50];
+    int i, soma = 0;
+    float media;
+    int acima_media = 0;
+
+    for (i = 0; i < 50; i++) {
+        printf("Digite a temperatura %d: ", i + 1);
+        scanf("%d", (int*)&temperaturas[i]);
+        soma += temperaturas[i];
+    }
+
+    media = soma / 50.0;
+
+    for (i = 0; i < 50; i++) {
+        if (temperaturas[i] > media) {
+            acima_media++;
+        }
+    }
+
+    printf("\nMédia das temperaturas: %.2f\n", media);
+    printf("Quantidade acima da média: %d\n", acima_media);
+
+    return 0;
+}
 
